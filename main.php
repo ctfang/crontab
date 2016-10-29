3464a4f96daff33673bbd0fa47b025ac85f175b7
selden1992 <yuanzhao97>
2016-10-29 20:07:31 +0800

    第一次

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 自动启用
+ * User: selden1992
+ * Date: 2016/10/29
+ * Time: 19:54
+ */
+
+\crontab\src\run::begin();
---
+++ src/run.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 主函数
+ * User: selden1992
+ * Date: 2016/10/29
+ * Time: 19:59
+ */
+
+namespace crontab\src;
+
+
+class run
+{
+    static protected $_config; // 配置单例
+    /**
+     * 启动函数
+     */
+    static public function begin()
+    {
+
+    }
+}
